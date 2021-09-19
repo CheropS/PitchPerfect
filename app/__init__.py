fb611906dd255f47e6e413ce73abb6d16deb955d
Sharon Cherop <sh.cherop@gmail.com>
2021-09-19 11:57:39 +0300

    initialized the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask 
+from flask import Flask, app 
 from flask_bootstrap import Bootstrap
+# from config import config_options
 
 
 bootstrap = Bootstrap()
 
+
 def create_app(config_name):
     app= Flask(__name__)
+    
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
-from .main import views
+
+    return app
+
+
+
