de37993791549955be238ce1063da6d0162fe3f3
Sharon Cherop <sh.cherop@gmail.com>
2021-09-20 06:53:23 +0300

    defined pitch class

---
+++ app/models.py
@@ -0,0 +1,10 @@
+
+class Pitch:
+    '''This is a class that defines Pitch class
+    '''
+
+    def __init__(self, id, category, title, description):
+        self.id=id
+        self.category=category
+        self.title=title
+        self.description=description
