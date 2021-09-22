475c0aa184ddf36ba28b15d6f2ffd4e89edc3280
Sharon Cherop <sh.cherop@gmail.com>
2021-09-22 16:02:30 +0300

    adding sql database to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, app 
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 # from config import config_options
 
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -12,6 +14,9 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    bootstrap.init_app(app)
+    db.init_app(app)
+
 
 
 
