b42439da442d2a011dc5c8f0bd047d087f4fb1ba
Sharon Cherop <sh.cherop@gmail.com>
2021-09-22 23:02:00 +0300

    uploading images by adding upload path and function

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
-# from config import config_options
+from .config import config_options
 
 
 bootstrap = Bootstrap()
@@ -11,14 +13,18 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app= Flask(__name__)
-    
-
+    app.config.from_object(config_options[config_name])
+    # app.config['SECRET_KEY']='secret'
     from .main import main as main_blueprint
-    from auth import auth
+    from .auth import auth
 
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
     app.register_blueprint(main_blueprint)
     bootstrap.init_app(app)
     db.init_app(app)
--- app/main/views.py
+++ app/main/views.py
@@ -4,8 +4,8 @@ from app.models import Pitch, User
 from flask import render_template
 from . import main
 from flask_login import login_required
-from flask import render_template, redirect, url_for, abort
-from ..import db
+from flask import render_template, redirect, url_for, abort, request
+from ..import db, photos
 
 # Views
 @main.route('/')
@@ -55,3 +55,14 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
