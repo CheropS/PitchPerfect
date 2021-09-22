2381b78497d7257583717ace818dcd761579b96b
Sharon Cherop <sh.cherop@gmail.com>
2021-09-22 15:31:32 +0300

    created route for main forms

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,5 @@
+from app.main.forms import PitchForms
+from app.models import Pitch
 from flask import render_template
 from . import main
 
@@ -9,3 +11,19 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/New_Pitch', methods=['POST', 'GET'])
+@login_required
+def newpitch():
+    form=PitchForms()
+    if form.validate_on_submit():
+        
+'''
+route:new pitch
+comments(id)
+user updates-profile
+user updates-picture
+upvote(pitch_id)
+downvote(pitch_id)
+
+'''
